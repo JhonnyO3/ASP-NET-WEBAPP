fee93200c3ce0d56bc50d625d57962e2234e5a78
JhonnyO3 <jhonatas2004@gmail.com>
2022-12-04 20:23:13 -0300

    projeto criado

---
+++ CSharp-MVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CSharp_MVC.Models;
+
+namespace CSharp_MVC.Controllers;
+
+public class HomeController : Controller
+{
+
+
+    public IActionResult Index()
+    {
+        HomeModel home = new HomeModel();
+        home.Email = "jhonatas2004";
+        home.Nome = "Jhonatas Oliveira da Silva";
+        return View(home);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ CSharp-MVC/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CSharp_MVC.Models
+{
+	public class HomeModel
+	{
+
+		public object Nome { get; set; }
+
+        public object Email { get; set; }
+
+    }
+}
+
